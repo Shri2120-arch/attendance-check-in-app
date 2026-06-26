dabc95179c2fd9a027a575e051a17dce4f3dba73
SHRI SUNDARAM AC <2503717610621054@cit.edu.in>
2026-06-26 07:46:07 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,16 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  eslint: {
+    // This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Dangerously allow production builds to successfully complete
+    // even if your project has Type errors.
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
